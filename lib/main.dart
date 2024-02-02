6436af40ad4ce4fb48c5085e489256b99c271780
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2024-02-02 13:16:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, Key? key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int value = 0;
+
+  void increment() {
+    setState(() {
+      value++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 157, 151, 247),
+      appBar: AppBar(
+        title: const Text(
+          "Ish App",
+          style: TextStyle(
+            color: Color.fromARGB(255, 241, 4, 19),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "you have pushed the button many times",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "$value",
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
