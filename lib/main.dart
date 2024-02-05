baa8e26d420414af0e5679c0aaa92266aa531ba3
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2024-02-05 05:37:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key, Key? key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
